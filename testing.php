da95f7479896e99766f7904feeb15ad54cbc2edf
krishnaapsingh <krishna.apsingh@gmail.com>
2017-09-04 13:34:38 +0530

    added news module

--- testing.php
+++ testing.php
@@ -7,6 +7,21 @@
 
 <?php   echo "testing"."new module added "."new code added"; ?>
 
+<module-one>
+  <ul>
+     <li>item1</li>
+	 <li>item2</li>
+	 <li>item3</li>
+	 <li>item4</li>
+	 <li>item5</li>
+  </ul>
+</module-one>
+
+<module-two>
+<div> my awesome new module about coding</div>
+</module-two>
+
+
 
 </body>
 
