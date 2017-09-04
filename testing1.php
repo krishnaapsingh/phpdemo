8baa6b5587ad1ca8d1c944f6d5a67e24f0d1c006
krishnaapsingh <krishna.apsingh@gmail.com>
2017-09-04 17:35:39 +0530

    commit feature123 code

---
+++ testing1.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>My Testing page</title>
+</head>
+
+<body>
+
+<?php   echo "testing"."new module added "."new code added"; ?>
+
+<module-one>
+  <ul>
+     <li>item1</li>
+	 <li>item2</li>
+	 <li>item3</li>
+	 <li>item4</li>
+	 <li>item5</li>
+  </ul>
+</module-one>
+
+<module-two>
+<div> my awesome new module about coding</div>
+</module-two>
+
+
+
+</body>
+
+
+</html>
